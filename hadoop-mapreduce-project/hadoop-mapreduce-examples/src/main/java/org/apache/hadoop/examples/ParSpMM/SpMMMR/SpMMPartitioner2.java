81a4d9bfe368473492ffe3db57a6ce3cc6cc852c
srkandul@ncsu.edu <srkandul@ncsu.edu>
2014-04-08 02:43:32 -0400

    print partitioner count

--- hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/ParSpMM/SpMMMR/SpMMPartitioner2.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/ParSpMM/SpMMMR/SpMMPartitioner2.java
@@ -8,8 +8,9 @@ public class SpMMPartitioner2 extends Partitioner<Key, Value> {
 
 	@Override
 	public int getPartition(Key key, Value value, int numPartitions) {
+		System.out.println("Partition count : " + numPartitions);
 		//strategy 2
-		if(key.m == 0)
+		if(key.m == 0) //A
 			return (key.index2 * SpMMDriver.SPMM_PROC_GRID_DIMM_X + key.index3) % numPartitions;
 		else
 			return (key.index1 * SpMMDriver.SPMM_PROC_GRID_DIMM_X + key.index3) % numPartitions;
