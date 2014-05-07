3ba86c0535adc0a1b3ed8138b4cf41e54a371145
srkandul@ncsu.edu <srkandul@ncsu.edu>
2014-05-07 03:39:04 -0400

    library pickup fix

--- hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/Kmeans/KMDriver.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/Kmeans/KMDriver.java
@@ -49,8 +49,7 @@ public class KMDriver {
 		     System.out.println("USAGE: <COUNT> <K> <DIMENSION OF VECTORS> <MAXITERATIONS> <num of tasks> <convgDelta>");
 		      return;
 		}
-
-		conf = new Configuration();
+		
 		int count = Integer.parseInt(remainingArgs[0]);
 		int k = Integer.parseInt(remainingArgs[1]);
 		int dimension = Integer.parseInt(remainingArgs[2]);
