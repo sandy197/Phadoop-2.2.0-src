8bfddd0ea08a35d21dfa528eef3cc80646edbfa7
srkandul@ncsu.edu <srkandul@ncsu.edu>
2014-06-06 13:11:50 -0400

    Coding for step 1)Send/Receive RAPLRecord between MapTask/MRAppMaster

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/job/impl/TaskAttemptImpl.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/job/impl/TaskAttemptImpl.java
@@ -40,8 +40,6 @@ import java.util.regex.Pattern;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.examples.MKmeans.Key;
-import org.apache.hadoop.examples.MKmeans.MKMTypes.Values;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
